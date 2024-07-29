ca15c6f9c9f26c932b2f8caeee23183636f2df1e
GurubasMaka <145183120+GurubasMaka@users.noreply.github.com>
2024-07-29 12:05:20 +0000

    conversion of datatypes & operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,3 +22,44 @@ let someNumber=33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+////////////////////////////////////////////////////////////
+
+///***** operations *******/
+let value=3
+let negValue =-value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-1);
+// console.log(2**3);
+let str1 ="hello"
+let str2 ="gurubas"
+
+let str3 =str1+str2;
+
+console.log(str3);
+
+// console.log("1" +2);  //12
+// console.log(1 + "2");  //12
+// console.log("1" +2 +2); //122
+
+// console.log(1 + 2 + "2");  //32
+// console.log(1+ "2"+3);  //123
+
+// console.log(true);
+// console.log(+true);  //1
+
+// console.log(+"");   //0
+
+let num1 ,num2, num3
+
+num1 = num2 =num3 =2+2
+
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
