f91e3fc977f445ab818252d5d8933bdba791f9ce
GurubasMaka <145183120+GurubasMaka@users.noreply.github.com>
2024-08-05 13:43:02 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,44 @@
+// console.log('H');
+// console.log('I');
+
+
+// function sayMyName(){
+//     console.log('g');
+// console.log('u');
+// console.log('r');
+// console.log('u');
+
+// }
+// // sayMyName()
+// function addTwonumber(number1,number2)
+// {
+//    let result=  number1+number2
+//    return result
+    
+// }
+
+// const result=addTwonumber(3,5)
+// console.log("result:" ,result);
+
+// function loginUserMessage(username){
+//     if(username===undefined){
+//         console.log("please enter the username");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
+// console.log(loginUserMessage());
+
+function calculateCartPrice(...num1)
+{
+    return num1
+}
+console.log(calculateCartPrice(200,400,500));
+
+
+
+
+
+
+
+
