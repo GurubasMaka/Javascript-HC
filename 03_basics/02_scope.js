14e7ec5579b740a2dc652fef59b2d07d7d551704
GurubasMaka <145183120+GurubasMaka@users.noreply.github.com>
2024-08-06 12:53:18 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,44 @@
+// var c=9000
+// if(true)
+// {
+//     let a =10
+//     const b=70
+//     var c=90
+// }
+// console.log(c);
+
+// function one(){
+
+//     const username="hitesh"
+
+//     function two()
+//     {
+//         const website="youtube"
+//         console.log(username);
+//     }
+//     //console.log(website);
+//     two()
+    
+// }
+// one()
+if(true)
+{
+    const username="hitesh"
+    if(username==="hitesh")
+    {
+        const website="youtube"
+        console.log(username+website);
+        
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+//// +++++++++++++++++
+function addone(num)
+{
+    return num+1
+}
+console.log(addone(5));
+
